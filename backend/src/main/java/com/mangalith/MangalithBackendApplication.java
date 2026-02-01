bdd8a89017188844ce5cfceca1abe625ec1bd238
Nestor Gutierrez <nestorg456k@outlook.com>
2026-02-01 17:32:30 -0500

    feat(backend): generar proyecto Spring Boot base y estructura inicial

---
+++ backend/src/main/java/com/mangalith/MangalithBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mangalith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangalithBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangalithBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mangalith/MangalithBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangalith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangalithBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
