542500c289021468c1f4b77cc9ef97d54b2aa318
Nestor Gutierrez <nestorg456k@outlook.com>
2026-02-08 22:30:26 -0500

    test: activar perfil 'test' en MangalithBackendApplicationTests

--- backend/src/test/java/com/mangalith/MangalithBackendApplicationTests.java
+++ backend/src/test/java/com/mangalith/MangalithBackendApplicationTests.java
@@ -2,8 +2,11 @@ package com.mangalith;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MangalithBackendApplicationTests {
 
 	@Test
